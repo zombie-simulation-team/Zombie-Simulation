0959ba152a8cffcc394a9d3fbf7560ccd6024d35
Gavin Chin <chingavin@gmail.com>
2019-03-31 16:27:10 -0400

    Initial Commit

---
+++ Source/World/World.cpp
@@ -0,0 +1,5 @@
+//
+// Am I god if I have created the World Class?
+//
+
+#include "World.h"
---
+++ Source/World/World.h
@@ -0,0 +1,14 @@
+//
+// Am I god if I have created the World Class?
+//
+
+#ifndef ZOMBIE_SIMULATION_WORLD_H
+#define ZOMBIE_SIMULATION_WORLD_H
+
+
+class World {
+
+};
+
+
+#endif //ZOMBIE_SIMULATION_WORLD_H
