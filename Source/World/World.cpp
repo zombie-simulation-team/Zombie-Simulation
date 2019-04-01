fdedbf5e22605b024b515a6d986f35c84cf129c5
Gavin Chin <chingavin@gmail.com>
2019-03-31 23:24:56 -0400

    included the World class

--- Source/World/World.cpp
+++ Source/World/World.cpp
@@ -1,5 +1,102 @@
 //
-// Am I god if I have created the World Class?
+// Am I god if I have created the World Class? - god
 //
 
 #include "World.h"
+
+//If no user configuration is set call the default constructor
+
+
+World::World()
+{
+    srand( (unsigned)time( NULL ) );
+    numOfContinents = rand() % 7 + 1;
+    elapsedDays = 0;
+    RandomContinentOrder();
+    continents = new Continents*[numOfContinents];
+    for(int i = 0; i < numOfContinents; i++){
+        continents[i] = new Continent(10, allContinentArr[i], rand()%5, rand()%5, rand()%5,rand()%5);
+    }
+}
+
+World::World(int numOfContinents){
+    srand( (unsigned)time( NULL ) );
+    this->numOfContinents = numOfContinents;
+    elapsedDays = 0;
+    RandomContinentOrder();
+    continents = new Continents*[numOfContinents];
+    for(int i = 0; i < numOfContinents; i++){
+        continents[i] = new Continent(10, allContinentArr[i], rand()%5, rand()%5, rand()%5,rand()%5);
+    }
+}
+
+World::World(Continents_e startingContinent){
+    srand( (unsigned)time( NULL ) );
+    numOfContinents = rand() % 7 + 1;
+    elapsedDays = 0;
+    RandomContinentOrder();
+    continents = new Continents*[numOfContinents];
+    for(int i = 0; i < numOfContinents; i++){
+        if(i == 0){
+            continents[i] = new Continent(10, startingContinent, rand()%5, rand()%5, rand()%5,rand()%5);
+        }else if(allContinentsArr[i] == startingContinent){
+            continents[i] = new Continent(10, allContinentArr[i+1], rand()%5, rand()%5, rand()%5,rand()%5);
+        }else{
+            continents[i] = new Continent(10, allContinentArr[i], rand()%5, rand()%5, rand()%5,rand()%5);
+        }
+    }
+}
+World::World(int numOfContinents, Continents_e startingContinent){
+    srand( (unsigned)time( NULL ) );
+    this->numOfContinents = numOfContinents;
+    elapsedDays = 0;
+    RandomContinentOrder();
+    continents = new Continents*[numOfContinents];
+    for(int i = 0; i < numOfContinents; i++){
+        if(i == 0){
+            continents[i] = new Continent(10, startingContinent, rand()%5, rand()%5, rand()%5,rand()%5);
+        }else if(allContinentsArr[i] == startingContinent){
+            continents[i] = new Continent(10, allContinentArr[i+1], rand()%5, rand()%5, rand()%5,rand()%5);
+        }else{
+            continents[i] = new Continent(10, allContinentArr[i], rand()%5, rand()%5, rand()%5,rand()%5);
+        }
+    }
+}
+
+World::GetDays(){
+    return elapsedDays;
+}
+
+World::SetDays(){
+    elapsedDays++;
+}
+
+World::Draw(){
+    //TODO: implement Draw
+}
+
+World::Tick(){
+    //TODO:implement Tick
+}
+
+//Returns the 7 continents in random order
+World::RandomContinentOrder(){
+
+   allContinentArr = { 
+        Africa,
+        Antartica,
+        Asia,
+        Australia,
+        Europe,
+        NorthAmerica,
+        SouthAmerica 
+    };
+   
+   for (int i = 0; i < 100; i++) {
+    int randElm1 = rand()%7;
+    int randElm2 = rand()%7;
+    Continents_e temp = allContinentArr[randElm];
+    allContinentArr[randElm] = allContinentArr[randElm2];
+    allContinentArr[randElm2] = temp;
+   }
+}
--- Source/World/World.h
+++ Source/World/World.h
@@ -1,12 +1,34 @@
 //
-// Am I god if I have created the World Class?
+// Am I god if I have created the World Class? - god
 //
 
 #ifndef ZOMBIE_SIMULATION_WORLD_H
 #define ZOMBIE_SIMULATION_WORLD_H
 
+#include "Continents.h" 
+#include "Environment.h"
+#include<string>
+#include <stdlib.h>  
+using namespace std; 
 
-class World {
+class World: public Environment {
+private:
+    int numOfContinents;
+    Continent ** continents;
+    int dayElapsed;
+    int continentCount;
+    Continents_e allContinentArr [7];
+    void randomContinentOrder();
+public:
+    World();
+    // This will be the user selected starting Continent
+    World(int numOfContinents);
+    World(Continents_e startingContinent);
+    World(int numOfContinents,Continents_e startingContinent);
+    int getDays();
+    void setDays();
+    void Draw();
+    void tick();
 
 };
 
