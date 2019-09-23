10c17655f60e17770a9a08d56af20e0dff1289dc
Javier <nanoterran@yahoo.com>
2019-09-23 14:09:34 -0400

    Added mock back

---
+++ Testing/Mock/RandomGenerator_Mock.cpp
@@ -0,0 +1,21 @@
+/*
+ * FakeRandomGenerator.cpp
+ *
+ *  Created on: Apr 5, 2019
+ *      Author: javiervega
+ */
+
+#include "RandomGenerator_Mock.h"
+
+RandomGenerator_Mock::RandomGenerator_Mock() {}
+
+RandomGenerator_Mock::~RandomGenerator_Mock() {}
+
+int RandomGenerator_Mock::GenerateRandom(int start, int end)
+{
+	return mock().actualCall("GenerateRandom")
+			.onObject(this)
+			.withParameter("start", start)
+			.withParameter("end", end)
+			.returnValue().getIntValue();
+}
